8bafceb5ca1bea741f414da0d7250c8f308c5520
Duc Tran <ducvtrann@gmail.com>
2020-09-11 12:29:43 -0400

    'Add packages to use async/await on client side'

--- server/index.js
+++ server/index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const path = require('path');
 const morgan = require('morgan');
+const cors = require('cors');
 
 // Constants
 const app = express();
@@ -18,6 +19,9 @@ const createApp = () => {
   // body parsing middleware
   app.use(express.json());
   app.use(express.urlencoded({ extended: true }));
+
+  // CORS
+  app.use(cors());
 
   // Main route
   require('./routes')(app);
