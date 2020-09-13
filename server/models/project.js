9d21efda90d73978100d2a23a58b9fc227752245
Duc Tran <ducvtrann@gmail.com>
2020-09-13 12:23:02 -0400

    Added association to Project and Todo model

--- server/models/project.js
+++ server/models/project.js
@@ -2,14 +2,14 @@
 const { Model } = require('sequelize');
 
 module.exports = (sequelize, DataTypes) => {
-  class Todo extends Model {
+  class Project extends Model {
     /**
      * Helper method for defining associations.
      * This method is not a part of Sequelize lifecycle.
      * The `models/index` file will call this method automatically.
      */
     static associate(models) {
-      Todo.belongsTo(models.Project, {
+      Project.hasMany(models.Todo, {
         foreignKey: {
           name: 'projectId',
           allowNull: true,
@@ -17,25 +17,17 @@ module.exports = (sequelize, DataTypes) => {
       });
     }
   }
-  Todo.init(
+  Project.init(
     {
-      content: {
+      name: {
         type: DataTypes.STRING,
         allowNull: false,
-      },
-      complete: {
-        type: DataTypes.BOOLEAN,
-        defaultValue: false,
-      },
-      projectId: {
-        type: DataTypes.INTEGER,
-        allowNull: true,
       },
     },
     {
       sequelize,
-      modelName: 'Todo',
+      modelName: 'Project',
     }
   );
-  return Todo;
+  return Project;
 };
